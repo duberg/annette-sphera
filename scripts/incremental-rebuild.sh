61af380f08b818dce4772c51192bcbc8dbcf12c8
David Kantemirov <it@kantemirov.ru>
2018-10-01 16:30:49 +0300

    Renamed keyspace

--- scripts/incremental-rebuild.sh
+++ scripts/incremental-rebuild.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 ~annette-sphera-server/reStart
+
+# angular config:
+# ~/Desktop/annette-sphera/annette-frontend-sphera/ng/package.json
+# run start
--- scripts/triggered-execution.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-~annette-sphera-server/reStart
